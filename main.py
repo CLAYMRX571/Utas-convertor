172d076644ce36300222abc49b5551d1fc705266
claymrx571 <claymrx571@gmail.com>
2026-03-16 14:18:20 +0500

    Utass

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
+from aiohttp import web
 
 from func import router
 
@@ -14,6 +15,22 @@ async def start():
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await dp.start_polling(bot)
+
+async def handle(request):
+    return web.Response(text="Bot ishlayapti")
+
+async def start_web():
+    app = web.Application()
+    app.router.add_get("/", handle)
+
+    port = int(os.environ.get("PORT", 10000))
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
+async def main():
+    await asyncio.gather(start(), start_web())
 
 if __name__ == "__main__":
     asyncio.run(start())
