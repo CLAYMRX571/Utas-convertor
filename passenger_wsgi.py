62e7e0644277936c875f795d91f8b4f609cae65a
claymrx571 <claymrx571@gmail.com>
2026-03-22 03:08:11 +0500

    pass

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,15 @@
-import sys, os
+import sys
+import os
 
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = "/home/claymrx1/e.utas.uz"
+VENV_PATH = "/home/claymrx1/virtualenv/e.utas.uz/3.13"
+
+sys.path.insert(0, VENV_PATH + "/lib/python3.13/site-packages")
+
+os.environ["PATH"] = VENV_PATH + "/bin:" + os.environ["PATH"]
+os.environ["VIRTUAL_ENV"] = VENV_PATH
+
 sys.path.insert(0, BASE_DIR)
+os.chdir(BASE_DIR)
 
-from webhook import application
+from webhook import app as application
